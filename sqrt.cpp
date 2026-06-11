b1f189400691cc74da2f11cf5f10a7b907c1f093
Brian-Bowser <bbows001@odu.edu>
2026-06-10 21:17:48 -0400

    Unzipped the file

---
+++ sqrt.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double estimateSqrt(double target, double initial)
+{
+    /*** Your code here ***/
+}
+
+int main()
+{
+    cout << "What number would you like the square root of? " << flush;
+
+    double z;
+    cin >> z;
+
+    cout << "Enter an initial guess: " << flush;
+
+    double x;
+    cin >> x;
+    if (x > 0.0 && z > 0.0)
+    {
+        double s = estimateSqrt(z, x);
+        cout << "The square root is " << fixed << setprecision(3) << s << endl;
+    }
+    else
+    {
+        cout << "Sorry, but I can't do that" << endl;
+    }
+
+    return 0;
+}
